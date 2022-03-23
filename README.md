7934d15e3825aff7e3ebc345bf0a86b33e29dfed
unknown <oqzcmcuxr@mozmail.com>
2022-03-23 20:05:44 +0300

    feat: typeprovider, validatorcompiler, serializercompiler

---
+++ README.md
@@ -0,0 +1,34 @@
+# Fastify Type Provider Zod
+
+## How to use?
+
+```js
+import Fastify from "fastify";
+import {serializerCompiler, validatorCompiler, ZodTypeProvider,} from "fastify-type-provider-zod";
+import z from "zod";
+
+const app = Fastify()
+
+// Add schema validator and serializer
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.withTypeProvider<ZodTypeProvider>().route({
+  method: "GET",
+  url: "/",
+  // Define your schema
+  schema: {
+    querystring: z.object({
+      name: z.string().min(4),
+    }),
+    response: {
+      200: z.string(),
+    },
+  },
+  handler: (req, res) => {
+    res.send(req.query.name);
+  },
+});
+
+app.listen({ port: 4949 });
+```
---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import { FastifySchemaCompiler, FastifyTypeProvider } from "fastify";
+import { FastifySerializerCompiler } from "fastify/types/schema";
+import { z, ZodAny, ZodTypeAny } from "zod";
+
+export interface ZodTypeProvider extends FastifyTypeProvider {
+  output: this["input"] extends ZodTypeAny ? z.infer<this["input"]> : never;
+}
+
+export const validatorCompiler: FastifySchemaCompiler<ZodAny> =
+  ({ schema }) =>
+  (data) => {
+    try {
+      return { value: schema.parse(data) };
+    } catch (error) {
+      return { error };
+    }
+  };
+
+export const serializerCompiler: FastifySerializerCompiler<ZodAny> =
+  ({ schema }) =>
+  (data) => {
+    const result = schema.safeParse(data);
+    if (result.success) {
+      return JSON.stringify(result.data);
+    }
+    throw Error("Response doesn't match the schema");
+  };
