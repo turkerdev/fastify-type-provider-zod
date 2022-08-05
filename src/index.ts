8d46a6dc8a0d2986072578d6bae9035b3db6a6c3
Igor Savin <iselwin@gmail.com>
2022-08-05 16:06:48 +0300

    Implement jsonSchemaTransform for fastify-swagger support

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
-import type { FastifySchemaCompiler, FastifyTypeProvider } from 'fastify';
+import type { FastifySchemaCompiler, FastifyTypeProvider, FastifySchema } from 'fastify';
 import type { FastifySerializerCompiler } from 'fastify/types/schema';
 import type { z, ZodAny, ZodTypeAny } from 'zod';
+import { zodToJsonSchema } from 'zod-to-json-schema';
 
 export interface ZodTypeProvider extends FastifyTypeProvider {
   output: this['input'] extends ZodTypeAny ? z.infer<this['input']> : never;
 }
+
+export const jsonSchemaTransform = ({ schema, url }: { schema: FastifySchema; url: string }) => {
+  const { params, body, querystring, headers, response } = schema;
+
+  const transformed: Record<string, any> = {};
+
+  if (params) transformed.params = zodToJsonSchema(params as any);
+  if (body) transformed.body = zodToJsonSchema(body as any);
+  if (querystring) transformed.querystring = zodToJsonSchema(querystring as any);
+  if (headers) transformed.headers = zodToJsonSchema(headers as any);
+  if (response) transformed.response = zodToJsonSchema(response as any);
+
+  return { schema: transformed, url };
+};
 
 export const validatorCompiler: FastifySchemaCompiler<ZodAny> =
   ({ schema }) =>
