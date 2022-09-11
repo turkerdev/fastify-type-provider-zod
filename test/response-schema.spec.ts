b3028b047ab35bdd2a45ebb27bcb055d2589a1de
Igor Savin <iselwin@gmail.com>
2022-09-11 20:39:21 +0300

    Fix support for empty responses in the schema (#20)

--- test/response-schema.spec.ts
+++ test/response-schema.spec.ts
@@ -6,6 +6,67 @@ import type { ZodTypeProvider } from '../src';
 import { serializerCompiler, validatorCompiler } from '../src';
 
 describe('response schema', () => {
+  describe('does not fail on empty response schema (204)', () => {
+    let app: FastifyInstance;
+    beforeAll(async () => {
+      app = Fastify();
+      app.setValidatorCompiler(validatorCompiler);
+      app.setSerializerCompiler(serializerCompiler);
+
+      app.after(() => {
+        app
+          .withTypeProvider<ZodTypeProvider>()
+          .route({
+            method: 'GET',
+            url: '/',
+            schema: {
+              response: {
+                204: z.undefined().describe('test'),
+              },
+            },
+            handler: (req, res) => {
+              res.status(204).send();
+            },
+          })
+          .route({
+            method: 'GET',
+            url: '/incorrect',
+            schema: {
+              response: {
+                204: z.undefined().describe('test'),
+              },
+            },
+            handler: (req, res) => {
+              res.status(204).send({ id: 1 });
+            },
+          });
+      });
+      await app.ready();
+    });
+
+    afterAll(async () => {
+      await app.close();
+    });
+
+    it('returns 204', async () => {
+      const response = await app.inject().get('/');
+
+      expect(response.statusCode).toBe(204);
+      expect(response.body).toEqual('');
+    });
+
+    it('throws on non-empty', async () => {
+      const response = await app.inject().get('/incorrect');
+
+      expect(response.statusCode).toBe(500);
+      expect(response.json()).toEqual({
+        error: 'Internal Server Error',
+        message: "Response doesn't match the schema",
+        statusCode: 500,
+      });
+    });
+  });
+
   describe('correctly processes response schema (string)', () => {
     let app: FastifyInstance;
     beforeAll(async () => {
