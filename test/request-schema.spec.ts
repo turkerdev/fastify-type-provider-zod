70cb9a10e8f69675f029b03b6f7a368b50ab19f4
Igor Savin <iselwin@gmail.com>
2023-11-27 16:24:25 +0200

    Support readonly entities

--- test/request-schema.spec.ts
+++ test/request-schema.spec.ts
@@ -5,13 +5,35 @@ import { z } from 'zod';
 import type { ZodTypeProvider } from '../src';
 import { serializerCompiler, validatorCompiler } from '../src';
 
-describe('response schema', () => {
+const REQUEST_SCHEMA = z.object({
+  name: z.string(),
+  readonlyId: z.preprocess((value) => {
+    if (Array.isArray(value)) {
+      return value;
+    }
+    return [value];
+  }, z.array(z.string().readonly())),
+});
+
+const schema = z.object({
+  foo: z.boolean(),
+  bar: z.number(),
+});
+
+const REQUEST_BODY_SCHEMA = z.object({
+  name: z.string(),
+  readonlyObject: z
+    .object({
+      id: z.string(),
+    })
+    .readonly(),
+  valueRoArray: schema.array().readonly(),
+  valueRo: schema.readonly(),
+});
+
+describe('request schema', () => {
   let app: FastifyInstance;
   beforeAll(async () => {
-    const REQUEST_SCHEMA = z.object({
-      name: z.string(),
-    });
-
     app = Fastify();
     app.setValidatorCompiler(validatorCompiler);
     app.setSerializerCompiler(serializerCompiler);
@@ -28,6 +50,22 @@ describe('response schema', () => {
           handler: (req, res) => {
             res.send({
               name: req.query.name,
+              readonlyId: req.query.readonlyId[0],
+            });
+          },
+        })
+        .route({
+          method: 'POST',
+          url: '/',
+          schema: {
+            body: REQUEST_BODY_SCHEMA,
+          },
+          handler: (req, res) => {
+            res.send({
+              name: req.body.name,
+              readonlyId: req.body.readonlyObject.id,
+              arrId: req.body.valueRoArray[0].bar,
+              objId: req.body.valueRo.bar,
             });
           },
         })
@@ -49,14 +87,49 @@ describe('response schema', () => {
     await app.close();
   });
 
-  it('accepts correct request', async () => {
-    const response = await app.inject().get('/').query({
-      name: 'test',
-    });
+  it('accepts correct request with query params', async () => {
+    const response = await app
+      .inject()
+      .get('/')
+      .query({
+        name: 'test',
+        readonlyId: ['ro'],
+      });
 
     expect(response.statusCode).toBe(200);
     expect(response.json()).toEqual({
       name: 'test',
+      readonlyId: 'ro',
+    });
+  });
+
+  it('accepts correct body', async () => {
+    const response = await app
+      .inject()
+      .post('/')
+      .body({
+        name: 'test',
+        readonlyObject: {
+          id: 'ro',
+        },
+        valueRoArray: [
+          {
+            foo: false,
+            bar: 0,
+          },
+        ],
+        valueRo: {
+          foo: true,
+          bar: 1,
+        },
+      });
+
+    expect(response.statusCode).toBe(200);
+    expect(response.json()).toEqual({
+      name: 'test',
+      readonlyId: 'ro',
+      arrId: 0,
+      objId: 1,
     });
   });
 
@@ -70,7 +143,9 @@ describe('response schema', () => {
   });
 
   it('returns 400 on validation error', async () => {
-    const response = await app.inject().get('/');
+    const response = await app.inject().get('/').query({
+      readonlyId: 'ro',
+    });
 
     expect(response.statusCode).toBe(400);
     expect(response.json()).toMatchSnapshot();
