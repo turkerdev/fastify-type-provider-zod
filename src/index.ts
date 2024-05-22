7db7920096287a3586656c3a880c4d8c12a44ea4
Bram-dc <bramdelcanho@gmail.com>
2024-05-22 15:08:27 +0200

    feat: added refs feat: separated type providers

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,8 @@ import type {
   RawServerDefault,
 } from 'fastify';
 import type { FastifySerializerCompiler } from 'fastify/types/schema';
-import type { ZodAny, ZodTypeAny, z } from 'zod';
+import type { OpenAPIV2, OpenAPIV3, OpenAPIV3_1 } from 'openapi-types';
+import type { z } from 'zod';
 import { zodToJsonSchema } from 'zod-to-json-schema';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -26,7 +27,8 @@ const defaultSkipList = [
 ];
 
 export interface ZodTypeProvider extends FastifyTypeProvider {
-  output: this['input'] extends ZodTypeAny ? z.infer<this['input']> : unknown;
+  validator: this['schema'] extends z.ZodTypeAny ? z.output<this['schema']> : unknown;
+  serializer: this['schema'] extends z.ZodTypeAny ? z.input<this['schema']> : unknown;
 }
 
 interface Schema extends FastifySchema {
@@ -37,6 +39,14 @@ const zodToJsonSchemaOptions = {
   target: 'openApi3',
   $refStrategy: 'none',
 } as const;
+
+export const jsonSchemaTransformSchemas = (inputSchemas: Record<string, z.ZodTypeAny>) => {
+  const schemas: Record<string, OpenAPIV3.SchemaObject | OpenAPIV3_1.SchemaObject> = {};
+  for (const key in inputSchemas)
+    schemas[key] = zodToJsonSchema(inputSchemas[key], zodToJsonSchemaOptions);
+
+  return schemas;
+};
 
 export const createJsonSchemaTransform = ({ skipList }: { skipList: readonly string[] }) => {
   return ({ schema, url }: { schema: Schema; url: string }) => {
@@ -97,31 +107,92 @@ export const jsonSchemaTransform = createJsonSchemaTransform({
   skipList: defaultSkipList,
 });
 
-export const validatorCompiler: FastifySchemaCompiler<ZodAny> =
-  ({ schema }) =>
+export const jsonSchemaTransformObject = (
+  input:
+    | { swaggerObject: Partial<OpenAPIV2.Document> }
+    | { openapiObject: Partial<OpenAPIV3.Document | OpenAPIV3_1.Document> },
+) => {
+  if ('swaggerObject' in input) {
+    console.warn('This package currently does not support component references for Swagger 2.0');
+    return input.swaggerObject;
+  }
+
+  const componentMapVK = new Map<string, string>();
+  Object.entries(input.openapiObject.components?.schemas ?? {}).forEach(([key, value]) =>
+    componentMapVK.set(JSON.stringify(value), key),
+  );
+
+  const RANDOM_COMPONENT_KEY_PREFIX = Math.random().toString(36).substring(2);
+
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  (data): any => {
-    try {
-      return { value: schema.parse(data) };
-    } catch (error) {
-      return { error };
+  const componentReplacer = (key: string, value: any) => {
+    if (typeof value !== 'object') return value;
+
+    if (key.startsWith(RANDOM_COMPONENT_KEY_PREFIX)) return value;
+
+    const stringifiedValue = JSON.stringify(value);
+    if (componentMapVK.has(stringifiedValue))
+      return { $ref: `#/components/schemas/${componentMapVK.get(stringifiedValue)}` };
+
+    if (value.nullable === true) {
+      const nonNullableValue = { ...value };
+      delete nonNullableValue.nullable;
+      const stringifiedNonNullableValue = JSON.stringify(nonNullableValue);
+      if (componentMapVK.has(stringifiedNonNullableValue))
+        return {
+          allOf: [
+            { $ref: `#/components/schemas/${componentMapVK.get(stringifiedNonNullableValue)}` },
+          ],
+          nullable: true,
+        };
+    }
+
+    return value;
+  };
+
+  const document = {
+    ...input.openapiObject,
+    components: {
+      ...input.openapiObject.components,
+      schemas: Object.fromEntries(
+        [...componentMapVK].map(([value, key]) => [
+          RANDOM_COMPONENT_KEY_PREFIX + key,
+          JSON.parse(value),
+        ]),
+      ),
+    },
+  };
+  const stringified = JSON.stringify(document, componentReplacer).replace(
+    new RegExp(RANDOM_COMPONENT_KEY_PREFIX, 'g'),
+    '',
+  );
+  const parsed = JSON.parse(stringified);
+
+  return parsed;
+};
+
+export const validatorCompiler: FastifySchemaCompiler<z.ZodAny> =
+  ({ schema }) =>
+  (data) => {
+    const result = schema.safeParse(data);
+    if (result.success) {
+      return { value: result.data };
+    } else {
+      return { error: result.error };
     }
   };
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function hasOwnProperty<T, K extends PropertyKey>(obj: T, prop: K): obj is T & Record<K, any> {
-  return Object.prototype.hasOwnProperty.call(obj, prop);
-}
-
-function resolveSchema(maybeSchema: ZodAny | { properties: ZodAny }): Pick<ZodAny, 'safeParse'> {
-  if (hasOwnProperty(maybeSchema, 'safeParse')) {
+const resolveSchema = (
+  maybeSchema: z.ZodAny | { properties: z.ZodAny },
+): Pick<z.ZodAny, 'safeParse'> => {
+  if ('safeParse' in maybeSchema) {
     return maybeSchema;
   }
-  if (hasOwnProperty(maybeSchema, 'properties')) {
+  if ('properties' in maybeSchema) {
     return maybeSchema.properties;
   }
   throw new Error(`Invalid schema passed: ${JSON.stringify(maybeSchema)}`);
-}
+};
 
 export class ResponseValidationError extends Error {
   public details: FreeformRecord;
@@ -133,10 +204,10 @@ export class ResponseValidationError extends Error {
   }
 }
 
-export const serializerCompiler: FastifySerializerCompiler<ZodAny | { properties: ZodAny }> =
+export const serializerCompiler: FastifySerializerCompiler<z.ZodAny | { properties: z.ZodAny }> =
   ({ schema: maybeSchema }) =>
   (data) => {
-    const schema: Pick<ZodAny, 'safeParse'> = resolveSchema(maybeSchema);
+    const schema: Pick<z.ZodAny, 'safeParse'> = resolveSchema(maybeSchema);
 
     const result = schema.safeParse(data);
     if (result.success) {
