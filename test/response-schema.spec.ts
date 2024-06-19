9a585b59536eae23ac8355e91091d59471d4310b
Dean Hayes <dean.hayes@statsbomb.com>
2024-06-19 17:26:48 +0100

    Add support for multiple response content types
    This commit adds support for different content type repsponses, as described
    here: https://github.com/fastify/fastify-swagger?#different-content-types-responses

--- test/response-schema.spec.ts
+++ test/response-schema.spec.ts
@@ -192,4 +192,102 @@ describe('response schema', () => {
       expect(response.json()).toMatchSnapshot();
     });
   });
+
+  describe('correctly processes different response schemas', () => {
+    let app: FastifyInstance;
+    beforeEach(async () => {
+      const REPLY_SCHEMA = z.object({
+        name: z.string(),
+      });
+      const TEXT_REPLY_SCHEMA = z.string();
+
+      app = Fastify();
+      app.setValidatorCompiler(validatorCompiler);
+      app.setSerializerCompiler(serializerCompiler);
+
+      app.after(() => {
+        app.withTypeProvider<ZodTypeProvider>().route({
+          method: 'GET',
+          url: '/',
+          schema: {
+            response: {
+              200: {
+                content: {
+                  'application/json': {
+                    schema: REPLY_SCHEMA,
+                  },
+                  'text/plain': {
+                    schema: TEXT_REPLY_SCHEMA,
+                  },
+                }
+              },
+            },
+          },
+          handler: (req, res) => {
+            if(req.headers.accept == 'application/json'){
+              res.send({
+                name: 'test',
+              });
+            }else{
+              res.send('test');
+            }
+          },
+        });
+
+        app.withTypeProvider<ZodTypeProvider>().route({
+          method: 'GET',
+          url: '/incorrect',
+          schema: {
+            response: {
+              200: {
+                content: {
+                  'application/json': {
+                    schema: REPLY_SCHEMA,
+                  },
+                  'text/plain': {
+                    schema: TEXT_REPLY_SCHEMA,
+                  },
+                }
+              },
+            },
+          },
+          handler: (req, res) => {
+            if(req.headers.accept == 'application/json'){
+              res.send('test');
+            }else{
+              res.send({
+                name: 'test',
+              });
+            }
+          },
+        });
+      });
+
+      await app.ready();
+    });
+    afterAll(async () => {
+      await app.close();
+    });
+
+    it('returns 200 for correct response', async () => {
+      let response = await app.inject({method: 'get', headers: {accept: 'application/json'}, url: '/' });
+      expect(response.statusCode).toBe(200);
+      expect(response.json()).toEqual({
+        name: 'test',
+      });
+
+      response = await app.inject({method: 'get', headers: {accept: 'text/plain'}, url: '/' });
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toEqual('test');
+    });
+
+    // FixMe https://github.com/turkerdev/fastify-type-provider-zod/issues/16
+    it.skip('returns 500 for incorrect response', async () => {
+      let response = await app.inject({method: 'get', headers: {accept: 'application/json'}, url: '/incorrect' });
+      expect(response.statusCode).toBe(500);
+
+      response = await app.inject({method: 'get', headers: {accept: 'text/plain'}, url: '/incorrect' });
+      expect(response.statusCode).toBe(500);
+    });
+  });
 });
