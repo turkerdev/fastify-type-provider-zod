bb03eeb7207b721fbe8c6d67b05dee56e9f957aa
Bram del Canho <bramdelcanho@gmail.com>
2025-04-13 13:21:00 +0200

    feat: Add createJsonSchemaTransformObject export to index.ts

--- index.ts
+++ index.ts
@@ -6,6 +6,7 @@ export {
   jsonSchemaTransform,
   createJsonSchemaTransform,
   jsonSchemaTransformObject,
+  createJsonSchemaTransformObject,
   serializerCompiler,
   validatorCompiler,
   createSerializerCompiler,
