5e49f3eb238ef637700d9b2cc6df7f256903e076
Marco Pasqualetti <24919330+marcalexiei@users.noreply.github.com>
2025-06-09 21:18:04 +0200

    fix: dist files not working correctly with node resolutions (#169)

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import UnpluginIsolatedDecl from 'unplugin-isolated-decl/vite'
+
+export default defineConfig({
+  build: {
+    lib: { entry: "src/index.ts",},
+    outDir: "dist",
+    emptyOutDir: true,
+    minify: false,
+    sourcemap: true,
+    rollupOptions: {
+      external: ["fastify", "zod/v4", "@fastify/swagger", "@fastify/error"],
+      output: [{
+        preserveModules: true,
+        entryFileNames: 'cjs/[name].cjs',
+        format: 'commonjs'
+      }, {
+        preserveModules: true,
+        entryFileNames: 'esm/[name].js',
+        format: 'es'
+      }],
+    },
+  },
+  plugins: [UnpluginIsolatedDecl()],
+});
